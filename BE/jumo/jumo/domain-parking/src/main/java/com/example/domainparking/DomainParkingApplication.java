e497eaea4082764debaaa4ff0c0200df77b4b7f0
luminaries1 <luminaries1@naver.com>
2023-04-19 14:39:27 +0900

    backend_init

---
+++ BE/jumo/jumo/domain-parking/src/main/java/com/example/domainparking/DomainParkingApplication.java
@@ -0,0 +1,13 @@
+package com.example.domainparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainParkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DomainParkingApplication.class, args);
+    }
+
+}

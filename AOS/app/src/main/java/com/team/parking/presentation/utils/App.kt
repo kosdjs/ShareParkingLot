2fec57a551d37b99b1ba9173b918069a8669972e
kimjihun <jihoon_story@naver.com>
2023-04-21 17:30:36 +0900

    feature-aos/map 개발중

--- AOS/app/src/main/java/com/team/parking/presentation/utils/App.kt
+++ AOS/app/src/main/java/com/team/parking/presentation/utils/App.kt
@@ -1,8 +1,16 @@
 package com.team.parking.presentation.utils
 
 import android.app.Application
+import com.naver.maps.map.NaverMapSdk
+import com.team.parking.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class App : Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+
+
+    }
 }

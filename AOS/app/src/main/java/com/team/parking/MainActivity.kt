8940e89ffd9ca0c91013fc52c10d8d249341ddff
kimjihun <jihoon_story@naver.com>
2023-04-24 15:32:35 +0900

    [AOS] 하단 네비게이션 바 삭제

--- AOS/app/src/main/java/com/team/parking/MainActivity.kt
+++ AOS/app/src/main/java/com/team/parking/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.team.parking
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
@@ -25,6 +29,7 @@ class MainActivity : AppCompatActivity() {
         setNavigationController()
         setNavigationDrawerInit()
         setOnClickNavigationDrawerItem()
+        setFullScreen()
     }
     
     fun setOnClickNavigationDrawerItem(){
@@ -57,5 +62,31 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fcv_activity_main) as NavHostFragment
         val navController = navHostFragment.navController
     }
+
+    /**
+     * 하단 네비게이션 삭제
+     */
+    fun setFullScreen(){
+        //Android 11(R) 대응
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.R){
+            supportActionBar?.hide()
+            window.setDecorFitsSystemWindows(false)
+            val controller = window.insetsController
+            if(controller!=null){
+                controller.hide(WindowInsets.Type.navigationBars())
+                controller.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+        }else{ //R버전 이하 대응
+            supportActionBar?.hide()
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN)
+        }
+    }
+
 
 }

04712e8aef23168e8d97852ca9cba654b0eede52
gun5046@naver.com <gun5046@naver.com>
2023-04-26 14:23:07 +0900

    Fix: [BE] 빈생성어노테이션 추가

--- BE/jumo/jumo-user/src/main/java/com/example/jumouser/factory/UserFactory.java
+++ BE/jumo/jumo-user/src/main/java/com/example/jumouser/factory/UserFactory.java
@@ -3,7 +3,9 @@ package com.example.jumouser.factory;
 import com.example.jumouser.provider.LoginProvider;
 import com.example.jumouser.provider.impl.KakaoLogin;
 import com.example.jumouser.provider.impl.NaverLogin;
+import org.springframework.stereotype.Component;
 
+@Component
 public class UserFactory {
 
     public LoginProvider loginSelector(String type){

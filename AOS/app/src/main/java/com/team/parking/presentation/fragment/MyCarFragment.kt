78db8dfdf300e3d1ce59e3c157136ae71fe66ab7
송관재 <kosdjs39@gmail.com>
2023-04-27 16:47:12 +0900

    Design: 차량 페이지 UI 작업

--- AOS/app/src/main/java/com/team/parking/presentation/fragment/MyCarFragment.kt
+++ AOS/app/src/main/java/com/team/parking/presentation/fragment/MyCarFragment.kt
@@ -5,29 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.team.parking.R
-import com.team.parking.databinding.FragmentProfileBinding
+import com.team.parking.databinding.FragmentMyCarBinding
 
-class ProfileFragment : Fragment() {
 
-    private lateinit var binding: FragmentProfileBinding
+class MyCarFragment : Fragment() {
+
+    private lateinit var binding: FragmentMyCarBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        binding = FragmentMyCarBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.imageBackProfile.setOnClickListener {
+        binding.imageBackMyCar.setOnClickListener {
             requireActivity().onBackPressed()
-        }
-        binding.textMainCarChangeProfile.setOnClickListener {
-            findNavController().navigate(R.id.action_profileFragment_to_myCarFragment)
         }
     }
 }

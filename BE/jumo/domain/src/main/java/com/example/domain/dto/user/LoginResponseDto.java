058d016677cf1076635fd2d5914698b6233e5510
gun5046@naver.com <gun5046@naver.com>
2023-04-28 17:59:15 +0900

    Log: [BE] 로그 추가

--- BE/jumo/domain/src/main/java/com/example/domain/dto/user/LoginResponseDto.java
+++ BE/jumo/domain/src/main/java/com/example/domain/dto/user/LoginResponseDto.java
@@ -1,4 +1,15 @@
 package com.example.domain.dto.user;
 
+import javax.persistence.Column;
+
 public class LoginResponseDto {
+    private Long user_id;
+    private String name;
+    private String type;
+
+    private String phone;
+    private String email;
+
+    private String profile_img;
+    private int ptHas;
 }
